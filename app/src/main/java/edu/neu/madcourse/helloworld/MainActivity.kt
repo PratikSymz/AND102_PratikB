e6c553811dd7ab3d4590acb7f781fccb97bfb370
pratiksymz28 <budhiraja.p@northeastern.edu>
2023-01-23 17:31:31 -0500

    Initial commit

---
+++ app/src/main/java/edu/neu/madcourse/helloworld/MainActivity.kt
@@ -0,0 +1,19 @@
+package edu.neu.madcourse.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Locate the button
+        val showButton = findViewById<Button>(R.id.view_button_show)
+        showButton.setOnClickListener {
+            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
